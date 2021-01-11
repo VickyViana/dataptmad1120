2c5ece09018fd5edab6884be500ded4a088d823f
VickyViana <mvictoria.viana@gmail.com>
2021-01-11 17:45:13 +0100

    lab-started

--- extra-work/m1/lab-errhand-listcomp/your-code/main.py
+++ extra-work/m1/lab-errhand-listcomp/your-code/main.py
@@ -8,13 +8,18 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
+import math
+import pandas as pd
+import os
+import random
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+square = [x**2 for x in range(20)]
+ 
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
@@ -27,26 +32,31 @@ print(my_listComprehension)
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
+sqrt =[math.sqrt(x) for x in range(100)]
 
-
+print(sqrt)
+                
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+my_list = [x for x in range(-10, 1)]
 
-
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
+odds =[x for x in range(100) if (x % 2) == 0]
 
-
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+divisible_by_seven =[x for x in range(1000) if (x % 7) == 0]
 
-
+print(divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
@@ -54,35 +64,48 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
+vowels = [A,E,I,O,U,a,e,i,o,u]
+non_vowels =[x for x in teststring if x not in vowels]
 
-
+print(non_vowels)
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+test_sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
 
+capital_letters =[x for x in test_sentence if x.isupper()]
 
+print(capital_letters)
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+sentence = 'The quick brown fox jumped over the lazy dog'
 
+consonants= [x for x in sentence if x not in vowels]
 
-
+print(consonants)
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+path = '/home/viki/Bootcamp'
+folders = [os.listdir(path)]
+
+print(folders)
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+random_lists = [random.sample(range(0,100),10) for x in range(4)]
 
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
@@ -90,7 +113,9 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
 
+flatten_list = [y for y in x for x in list_of_lists]
 
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -99,8 +124,9 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
+floats = float(list_of_lists)
 
-
+print(floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
